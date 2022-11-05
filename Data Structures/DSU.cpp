da0980b9ba8bbc7b947da298af246d9921d857da
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-05 06:32:06 +0200

    Update DSU.cpp

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -7,24 +7,35 @@ public:
         num_sets = N;
         rank.assign(N, 0);
         p.assign(N, 0);
-        set_size.assign(N, 1); //each set is initially 1
+        set_size.assign(N, 1); // each set is initially 1
         for (int i = 0; i < N; i++) {
             p[i] = i;
         }
     }
 
-    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
+    int findSet(int i)
+    { 
+        return (p[i] == i) ? i : (p[i] = findSet(p[i])); 
+    }
 
-    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
+    bool isSameSet(int i, int j)
+    {
+        return findSet(i) == findSet(j);
+    }
 
-    void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) {
+    void unionSet(int i, int j)
+    {
+        if (!isSameSet(i, j)) 
+        {
             num_sets--;
             int x = findSet(i), y = findSet(j);
-            if (rank[x] > rank[y]) {
+            if (rank[x] > rank[y]) 
+            {
                 p[y] = x;
                 set_size[x] += set_size[y];
-            } else {
+            } 
+            else 
+            {
                 p[x] = y;
                 set_size[y] += set_size[x];
                 if (rank[x] == rank[y]) rank[y]++;
@@ -32,7 +43,14 @@ public:
         }
     }
 
-    int num() { return num_sets; }
+    int num() 
+    { 
+        return num_sets;
+    }
 
-    int size(int i) { return set_size[findSet(i)]; }
+ 
+    int size(int i) 
+    { 
+        return set_size[findSet(i)];
+    }
 };
