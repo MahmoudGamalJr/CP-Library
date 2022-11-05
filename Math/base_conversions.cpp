9eea177522827154268ec06d9871d9f4e6cf3916
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-05 06:28:01 +0200

    Add files via upload

---
+++ Math/base_conversions.cpp
@@ -0,0 +1,24 @@
+string letters = "0123456789ABCDEF"; //hexadecimal
+int toInt(char c){
+	return letters.find(c);
+}
+
+int fromAnyBaseToDecimal(string& in, int base){
+	int ret = 0;
+	for(int i = 0; i<in.size(); i++){
+		ret *= base, ret += toInt(in[i]);
+	}
+	return ret;
+}
+
+string fromDecimalToAnybase(int number, int base){
+	if(number == 0) return "0";
+	string ret = "";
+
+	while(number){
+		ret = letters[number % base] + ret;
+		number/= base;
+	}
+
+	return ret;
+}
