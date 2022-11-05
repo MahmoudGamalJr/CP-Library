b4c19823996c13ea80cdf2ad82c95c8108c13ba0
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-05 06:35:40 +0200

    Update dfs.cpp

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -5,8 +5,10 @@ int N;
 vector<vector<int> > adj;
 vector<bool> vis;
 
-void dfs(int u) {
-    for (int v:adj[u]) {
+void dfs(int u) 
+{
+    for (int v:adj[u]) 
+    {
         if (!vis[v]) {
             vis[v] = 1;
             dfs(v);
@@ -17,7 +19,9 @@ void dfs(int u) {
 int main() {
     adj.resize(N);
     vis.assign(N, false);
-    for (int i = 0; i < N; ++i) {   //preTraversal:
+    for (int i = 0; i < N; ++i)
+    {   
+        //preTraversal:
         if (!vis[i]) dfs(i);         //for non-connected graphs, Spanning Forest
     }
     return 0;
