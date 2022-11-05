f13d7fd50039482fa612d6bc14adcd8561074a0c
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-05 06:43:16 +0200

    Update miscellaneous.cpp

--- Math/miscellaneous.cpp
+++ Math/miscellaneous.cpp
@@ -1,5 +1,5 @@
 //performing fast b^p
-ll fastpow(ll b, ll p) {
+long long fastpow(long long b, long long p) {
     if (p == 0) return 1;
     if (p & 1)
         return b * fastpow(b * b, p >> 1);
@@ -19,14 +19,14 @@ int lcm(int a, int b) {
 }
 
 // for solving ax+by=gcd(a,b) given a & b
-ll extendedeuclid(ll a, ll b, ll &x, ll &y) {
+long long extendedeuclid(long long a, long long b, long long &x, long long &y) {
     if (a < 0) {
-        ll r = extendedeuclid(-a, b, x, y);
+        long long r = extendedeuclid(-a, b, x, y);
         x *= -1;
         return r;
     }
     if (b < 0) {
-        ll r = extendedeuclid(a, -b, x, y);
+        long long r = extendedeuclid(a, -b, x, y);
         y *= -1;
         return r;
     }
@@ -36,26 +36,26 @@ ll extendedeuclid(ll a, ll b, ll &x, ll &y) {
         return a;
     }
 
-    ll g = extendedeuclid(b, a % b, y, x);
+    long long g = extendedeuclid(b, a % b, y, x);
     y -= (a / b) * x;
     return g;
 }
 
 // for solving ax+by=c given a,b & c
-ll diophantine(ll a, ll b, ll c, ll &x, ll &y, bool &found) {
-    ll g = extendedeuclid(a, b, x, y);
+long long diophantine(long long a, long long b, long long c, long long &x, long long &y, bool &found) {
+    long long g = extendedeuclid(a, b, x, y);
     if ((found = (c % g == 0))) x *= c / g, y *= c / g;
     return g;
 }
 
 
 //chinese remainder theorem to solve system of congruence equations
-ll crt(vector<ll> &rems, vector<ll> &mods) {
-    ll rem = rems[0], mod = mods[0];
+long long crt(vector<long long> &rems, vector<ll> &mods) {
+    long long rem = rems[0], mod = mods[0];
 
     for (int i = 1; i < rems.size(); i++) {
-        ll x, y, found, a = mod, b = -mods[i], c = rems[i] - rem;
-        ll g = diophantine(a, b, c, x, y, found);
+        long long x, y, found, a = mod, b = -mods[i], c = rems[i] - rem;
+        long long g = diophantine(a, b, c, x, y, found);
         if (!found) return -1;
         rem += mod * x;          //evaluate previous congruence
         rem = mod / g * mods[i]; //merge mods with previous equation
